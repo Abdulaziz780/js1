f843a8fd8c9b33873f1e7906336ad791228ad7cd
Abdulaziz780 <akhusanov410@gmail.com>
2024-03-07 19:57:50 +0500

    qiyin ekan

---
+++ main.js
@@ -0,0 +1,7 @@
+let ism = prompt('Ismingizni kiriting')
+let familiya = prompt('Familiya kiriting')
+let yosh = prompt('Yoshingizni kiriting')
+let fan = prompt('Yoqtirgan fanni kiriting')
+let mahalla = prompt('Yashidigon mahallani kiriting')
+
+console.log(`Ism: ${ism} \n Familiya: ${familiya} \n Yosh: ${yosh} \n Fan: ${fan} \n Mahalla: ${mahalla}`);
